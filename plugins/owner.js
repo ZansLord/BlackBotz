0c5775fafc32db48f8d7bfdd901d4d2ece2883dc
ZansLord <90433321+ZansLord@users.noreply.github.com>
2021-11-15 12:19:31 +0700

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
-  // F this.sendContact(m.chat, '6283144570377', 'RakuChan', m)
-stc = conn.sendContact(m.chat, '6285718366706', 'ZANS BOTZ', m)
+  // F this.sendContact(m.chat, '6285869074622', 'AbdulZans', m)
+stc = conn.sendContact(m.chat, '6285869074622', 'AbdulZans', m)
   conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*Developer Z A N S  B O T *', 'status@broadcast')
 }
 handler.command = ['owner']
